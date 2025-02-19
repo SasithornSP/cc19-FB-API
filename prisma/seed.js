145019341245dc91771f64e2a4c65b0f04ad0b60
SasithornSP <S.pinpitak@gmail.com>
2025-02-19 09:33:16 +0700

    update_seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,30 +4,22 @@ const bcrypt=require("bcryptjs")
 const hashdePassword = bcrypt.hashSync("123456",10)
 const userData = [
     {
-        firstname:"Aommy",
-        lastname:"CC19",
-        email:"aommy@gmail.com",
-        password:hashdePassword
+     firstname: 'Andy', lastname: 'Codecamp', password: hashdePassword, email: 'andy@ggg.mail',
+     profileImage: 'https://www.svgrepo.com/show/420364/avatar-male-man.svg'
     },
     {
-    firstname: 'Bobby',
-    lastname : 'Codecamp',
-    email : 'bobby@ggg.mail',
-    password : hashdePassword
-   },
-   {
-    firstname: 'Candy',
-    lastname : 'Codecamp',
-    mobile : '1111111111',
-    password : hashdePassword
-   },
-   {
-    firstname: 'Danny',
-    lastname : 'Codecamp',
-    mobile : '2222222222',
-    password : hashdePassword
-   }
-]
+     firstname: 'Bobby', lastname: 'Codecamp', password: hashdePassword, email: 'bobby@ggg.mail',
+     profileImage: 'https://www.svgrepo.com/show/420319/actor-chaplin-comedy.svg'
+    },
+    {
+     firstname: 'Candy', lastname: 'Codecamp', password: hashdePassword, mobile: '1111111111',
+     profileImage: 'https://www.svgrepo.com/show/420327/avatar-child-girl.svg'
+    },
+    {
+     firstname: 'Danny', lastname: 'Codecamp', password: hashdePassword, mobile: '2222222222',
+     profileImage: 'https://www.svgrepo.com/show/420314/builder-helmet-worker.svg'
+    },
+   ]
 //npx prisma db seed
 console.log('DB seed...');
 
